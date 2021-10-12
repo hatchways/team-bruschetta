822b64c69bd8ff256bd1265806d61f5b43fa0740
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-12 23:06:14 +0100

    profileSchema fully setup

--- server/models/Profile.ts
+++ server/models/Profile.ts
@@ -6,3 +6,19 @@ interface ProfileInterface {
   description: string;
   availability: string;
 }
+
+const profileSchema: ProfileInterface = new mongoose.Schema({
+  firstName: {
+    required: true,
+  },
+  lastName: {
+    required: true,
+  },
+  description: {
+    required: true
+  },
+  availability: {
+    required: true
+  }
+});
+
