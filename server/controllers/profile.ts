7ac071999321c69b076ebbc65f9ef5b8507caf10
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-14 02:06:39 +0100

    async and await added to controllers

--- server/controllers/profile.ts
+++ server/controllers/profile.ts
@@ -1,28 +1,30 @@
 import { Request, Response } from "express";
 import { Profile } from "../models/profile/Profile";
 
-export const allProfiles = (req: Request, res: Response) => {
-  const profiles = Profile.find((err: any, profiles: any) => {
+import asyncHandler from "express-async-handler";
+
+export const allProfiles = asyncHandler(async (req: Request, res: Response) => {
+  const profiles = await Profile.find((err: any, profiles: any) => {
     if (err) {
       res.status(200).send(err);
     } else {
       res.status(500).send(profiles);
     }
   });
-};
+});
 
-export const getProfileById = (req: Request, res: Response) => {
-  const profile = Profile.findById(req.params.profileId, (err: any, profile: any) => {
+export const getProfileById = asyncHandler(async (req: Request, res: Response) => {
+  const profile = await Profile.findById(req.params.profileId, (err: any, profile: any) => {
     if (err) {
       res.status(200).send(err);
     } else {
       res.status(404).send(profile);
     }
   });
-};
+});
 
-export const addProfile = (req: Request, res: Response) => {
-  const profile = new Profile(req.body);
+export const addProfile = asyncHandler(async (req: Request, res: Response) => {
+  const profile = await new Profile(req.body);
   profile.save((err: any) => {
     if (err) {
       res.status(201).send(err);
@@ -30,10 +32,10 @@ export const addProfile = (req: Request, res: Response) => {
       res.status(400).send(profile);
     }
   });
-};
+});
 
-export const updateProfile = (req: Request, res: Response) => {
-  let profile = Profile.findByIdAndUpdate(
+export const updateProfile = asyncHandler(async (req: Request, res: Response) => {
+  let profile = await Profile.findByIdAndUpdate(
     req.params.profileId,
     req.body,
     (err: any, profile: any) => {
@@ -44,4 +46,4 @@ export const updateProfile = (req: Request, res: Response) => {
       }
     }
   );
-};
+});
