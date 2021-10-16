711f117e7b0137653076043e5721c2f8cf2c9479
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-16 20:57:13 +0100

    Unused codes deleted & Array iterator used for DoB

--- client/src/pages/Profile/EditProfile.tsx
+++ client/src/pages/Profile/EditProfile.tsx
@@ -10,10 +10,8 @@ import {
   Box,
   Typography,
   FormControl,
-  InputLabel,
   Container,
 } from '@material-ui/core';
-// import { DatePicker } from '@material-ui/pickers/DatePicker';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import useStyles from './useStyles';
 
@@ -26,13 +24,18 @@ export default function EditProfile() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      firstName: data.get('firstName'),
-      lastName: data.get('lastName'),
-    });
   };
+
+  const months = Array.from({ length: 12 }, (item, index) => {
+    return new Date(0, index).toLocaleString('en-US', { month: 'long' });
+  }) as Array<string>;
+
+  const curr = new Date().getUTCFullYear();
+  const years = Array(curr - (curr - 70))
+    .fill('')
+    .map((v, idx) => curr - idx) as Array<number>;
+
+  const days = Array.from({ length: 31 }, (item, index) => index + 1) as Array<number>;
 
   const handleToggle = () => {
     setPhoneNumber(!phoneNumber);
@@ -74,135 +77,59 @@ export default function EditProfile() {
             <Grid item xs={12} sm={6} className={classes.field}>
               <div className={classes.gender}>Gender</div>
               <FormControl fullWidth className={classes.dropdown}>
-                <InputLabel id="gender">Male</InputLabel>
                 <Select
                   labelId="gender"
                   id="gender"
-                  // value={age}
-                  label="gender"
-                  // onChange={handleChange}
+                  label="Male"
+                  name="gender"
                   variant="outlined"
                   className={classes.select}
                 >
                   <MenuItem value={1}>Male</MenuItem>
                   <MenuItem value={2}>Female</MenuItem>
+                  <MenuItem value={3}>Do not Wish To Answer</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
           </Grid>
-          <Grid container spacing={3} className={classes.field}>
+          <Grid container spacing={2} className={classes.field}>
             <div className={classes.dob}>Date of Birth</div>
-            <Grid item xs={9} sm={4}>
+            <Grid item xs={10} sm={4}>
               <FormControl fullWidth className={classes.dropdown}>
-                <InputLabel id="month">June</InputLabel>
-                <Select
-                  labelId="month"
-                  id="month"
-                  // value={age}
-                  label="month"
-                  // onChange={handleChange}
-                  variant="outlined"
-                >
-                  <MenuItem value={1}>January</MenuItem>
-                  <MenuItem value={2}>February</MenuItem>
-                  <MenuItem value={3}>March</MenuItem>
-                  <MenuItem value={4}>April</MenuItem>
-                  <MenuItem value={5}>May</MenuItem>
-                  <MenuItem value={6}>June</MenuItem>
-                  <MenuItem value={7}>July</MenuItem>
-                  <MenuItem value={8}>August</MenuItem>
-                  <MenuItem value={9}>September</MenuItem>
-                  <MenuItem value={10}>October</MenuItem>
-                  <MenuItem value={11}>November</MenuItem>
-                  <MenuItem value={12}>December</MenuItem>
-                </Select>
-                {/* <DatePicker views={['year']} label="Year only" /> */}
-              </FormControl>
-
-              {/* <FormHelperText>Required</FormHelperText> */}
-            </Grid>
-            <Grid item xs={9} sm={1}>
-              <FormControl fullWidth className={classes.dropdown}>
-                <InputLabel id="day">15</InputLabel>
-                <Select
-                  labelId="day"
-                  id="day"
-                  // value={age}
-                  label="day"
-                  // onChange={handleChange}
-                  variant="outlined"
-                >
-                  <MenuItem value={1}>1</MenuItem>
-                  <MenuItem value={2}>2</MenuItem>
-                  <MenuItem value={3}>3</MenuItem>
-                  <MenuItem value={4}>4</MenuItem>
-                  <MenuItem value={5}>5</MenuItem>
-                  <MenuItem value={6}>6</MenuItem>
-                  <MenuItem value={7}>7</MenuItem>
-                  <MenuItem value={8}>8</MenuItem>
-                  <MenuItem value={9}>9</MenuItem>
-                  <MenuItem value={10}>10</MenuItem>
-                  <MenuItem value={11}>11</MenuItem>
-                  <MenuItem value={12}>12</MenuItem>
-                  <MenuItem value={13}>13</MenuItem>
-                  <MenuItem value={14}>14</MenuItem>
-                  <MenuItem value={15}>15</MenuItem>
-                  <MenuItem value={16}>16</MenuItem>
-                  <MenuItem value={17}>17</MenuItem>
-                  <MenuItem value={18}>18</MenuItem>
-                  <MenuItem value={19}>19</MenuItem>
-                  <MenuItem value={20}>20</MenuItem>
-                  <MenuItem value={21}>21</MenuItem>
-                  <MenuItem value={22}>22</MenuItem>
-                  <MenuItem value={23}>23</MenuItem>
-                  <MenuItem value={24}>24</MenuItem>
-                  <MenuItem value={25}>25</MenuItem>
-                  <MenuItem value={26}>26</MenuItem>
-                  <MenuItem value={27}>27</MenuItem>
-                  <MenuItem value={28}>28</MenuItem>
-                  <MenuItem value={29}>29</MenuItem>
-                  <MenuItem value={30}>30</MenuItem>
-                  <MenuItem value={31}>31</MenuItem>
+                <Select labelId="month" id="month" name="month" label="June" variant="outlined">
+                  {months.map((eachMonth) => {
+                    return (
+                      <MenuItem value={eachMonth} key={eachMonth}>
+                        {eachMonth}
+                      </MenuItem>
+                    );
+                  })}
                 </Select>
               </FormControl>
             </Grid>
-            <Grid item xs={9} sm={4}>
+            <Grid item xs={10} sm={2}>
               <FormControl fullWidth className={classes.dropdown}>
-                <InputLabel id="year">1988</InputLabel>
-                <Select
-                  labelId="year"
-                  id="year"
-                  // value={age}
-                  label="year"
-                  // onChange={handleChange}
-                  variant="outlined"
-                >
-                  <MenuItem value={1980}>1980</MenuItem>
-                  <MenuItem value={1981}>1981</MenuItem>
-                  <MenuItem value={1982}>1982</MenuItem>
-                  <MenuItem value={1983}>1983</MenuItem>
-                  <MenuItem value={1984}>1984</MenuItem>
-                  <MenuItem value={1985}>1985</MenuItem>
-                  <MenuItem value={1986}>1986</MenuItem>
-                  <MenuItem value={1987}>1987</MenuItem>
-                  <MenuItem value={1988}>1988</MenuItem>
-                  <MenuItem value={1989}>1989</MenuItem>
-                  <MenuItem value={1990}>1990</MenuItem>
-                  <MenuItem value={1991}>1991</MenuItem>
-                  <MenuItem value={1992}>1992</MenuItem>
-                  <MenuItem value={1993}>1993</MenuItem>
-                  <MenuItem value={1994}>1994</MenuItem>
-                  <MenuItem value={1995}>1995</MenuItem>
-                  <MenuItem value={1996}>1996</MenuItem>
-                  <MenuItem value={1997}>1997</MenuItem>
-                  <MenuItem value={1998}>1998</MenuItem>
-                  <MenuItem value={1999}>1999</MenuItem>
-                  <MenuItem value={2000}>2000</MenuItem>
-                  <MenuItem value={2001}>2001</MenuItem>
-                  <MenuItem value={2002}>2002</MenuItem>
-                  <MenuItem value={2003}>2003</MenuItem>
-                  <MenuItem value={2004}>2004</MenuItem>
-                  <MenuItem value={2005}>2005</MenuItem>
+                <Select labelId="day" id="day" name="day" label="15" variant="outlined">
+                  {days.map((day) => {
+                    return (
+                      <MenuItem value={day} key={day}>
+                        {day}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={10} sm={3}>
+              <FormControl fullWidth className={classes.dropdown}>
+                <Select labelId="year" id="year" name="year" label="1988" variant="outlined">
+                  {years.map((year) => {
+                    return (
+                      <MenuItem value={year} key={year}>
+                        {year}
+                      </MenuItem>
+                    );
+                  })}
                 </Select>
               </FormControl>
             </Grid>
@@ -233,7 +160,6 @@ export default function EditProfile() {
                   onClick={handleToggle}
                 >
                   {phoneNumber ? 'Save Phone Number' : 'Add a Phone Number'}
-                  {/* Add a Phone Number */}
                 </Button>
               </Grid>
             </Grid>
