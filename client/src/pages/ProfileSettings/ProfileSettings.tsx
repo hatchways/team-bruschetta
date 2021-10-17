be1fb2ec599d84e890ee8e7a1d87f1bb9ef6a08f
Zorba11 <zorbathegreek111@gmail.com>
2021-10-16 19:23:55 -0600

    removed navar and sidemenu imports and comments

--- client/src/pages/ProfileSettings/ProfileSettings.tsx
+++ client/src/pages/ProfileSettings/ProfileSettings.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-import NavBar from '../../components/NavBar/NavBar';
-import SideDraw from '../../components/SideDrawer/SideDraw';
+import React from 'react';
 import PhotoUpload from '../../components/PhotoUpload/PhotoUpload';
 import useStyles from './useStyles';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -16,14 +14,11 @@ const ProfileSettings = (): JSX.Element => {
   if (loggedInUser === undefined) return <CircularProgress />;
   if (!loggedInUser) {
     history.push('/login');
-    // loading for a split seconds until history.push works
     return <CircularProgress />;
   }
 
   return (
     <div className={classes.root}>
-      {/* <NavBar /> */}
-      {/* <SideDraw /> */}
       <PhotoUpload />
     </div>
   );
