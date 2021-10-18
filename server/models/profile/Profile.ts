b2e581c370c045fbaaed5aa32f9b58089c62279e
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-18 14:20:36 +0100

    dob adjusted, user iD linked and more validations

--- server/models/profile/Profile.ts
+++ server/models/profile/Profile.ts
@@ -7,38 +7,59 @@ export const ProfileSchema: Schema = new Schema({
   firstName: {
    type: String,
    required: true,
+   minlength : 2,
+   trim : true,
   },
   lastName: {
    type: String,
    required: true,
+   minlength : 2,
+   trim : true,
   },
   gender: {
    type: String,
    required: true,
   },
-  dob: {
-   required: true,
-  },
+  dob: [{
+    day: {
+      type: Number,
+      required: true, 
+    },
+    month: {  
+      type: String,
+      required: true, 
+    },
+    year: {  
+      type: Number,
+      required: true, 
+    },
+  }],
   email: {
    type: String,
    required: true,
+   trim : true,
   },
   phone: {
    type: Number,
    required: true,
+   minlength: 10,
+   trim : true,
   },
   address: {
    type: String,
    required: true,
+   trim : true,
   },
   description: {
    type: String,
-   required: true
+   required: true,
+   trim : true,
   },
   availability: {
    type: String,
-   required: true
-  }
+   required: true,
+  },
+  user: {type: Schema.Types.ObjectId, ref: "user"},
 });
 
 async function findOneOrCreate(
--- server/models/profile/profileInterface.ts
+++ server/models/profile/profileInterface.ts
@@ -2,7 +2,7 @@ export interface ProfileInterface {
   firstName: string;
   lastName: string;
   gender: string;
-  dob: any;
+  dob: any[];
   email: string;
   phone: number;
   address: string;
