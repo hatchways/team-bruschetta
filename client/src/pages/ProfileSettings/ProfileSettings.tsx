1968cdec168e83186e400760cabd149d7519627f
Zorba11 <zorbathegreek111@gmail.com>
2021-10-19 10:21:07 -0600

    loggedin check rmvd from profilesettings

--- client/src/pages/ProfileSettings/ProfileSettings.tsx
+++ client/src/pages/ProfileSettings/ProfileSettings.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import PhotoUpload from '../../components/PhotoUpload/PhotoUpload';
 import useStyles from './useStyles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { useHistory } from 'react-router-dom';
-import { useAuth } from '../../context/useAuthContext';
 
 const ProfileSettings = (): JSX.Element => {
   const classes = useStyles();
-
-  const { loggedInUser } = useAuth();
-  const history = useHistory();
-
-  if (loggedInUser === undefined) return <CircularProgress />;
-  if (!loggedInUser) {
-    history.push('/login');
-    return <CircularProgress />;
-  }
 
   return (
     <div className={classes.root}>
