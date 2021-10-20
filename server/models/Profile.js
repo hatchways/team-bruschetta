a7ac58bbffb12a988ae318012c8e96f6a7cad062
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-20 04:39:20 +0100

    modify dateOfBirth type to Date and camelCase used

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,72 +1,50 @@
-const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
-const ProfileSchema = new mongoose.Schema({
+const profileSchema = new mongoose.Schema({
+
+  user: {type: Schema.Types.ObjectId,
+    ref: "user"},
   firstName: {
-   type: String,
-   required: true,
-   minlength : 2,
-   trim : true,
+    type: String,
+    required: true,
+    minlength : 3,
+    trim : true,
   },
   lastName: {
-   type: String,
-   required: true,
-   minlength : 2,
-   trim : true,
+    type: String,
+    required: true,
+    minlength : 3,
+    trim : true,
   },
   gender: {
-   type: String,
-   required: true,
+    type: String,
+    required: true,
   },
-  dob: [{
-    day: {
-      type: Number,
-      required: true, 
-    },
-    month: {  
-      type: String,
-      required: true, 
-    },
-    year: {  
-      type: Number,
-      required: true, 
-    },
-  }],
-  email: {
-   type: String,
-   required: true,
-   trim : true,
+  dateOfBirth: {
+    type: Date,
+    required: true,
   },
+  email: {type: Schema.Types.ObjectId, 
+    ref: "user"},
   phone: {
-   type: Number,
-   required: true,
-   minlength: 10,
-   trim : true,
+    type: Number,
+    required: true,
+    minlength: 10,
+    trim : true,
   },
   address: {
-   type: String,
-   required: true,
-   trim : true,
+    type: String,
+    required: true,
+    trim : true,
   },
   description: {
-   type: String,
-   required: true,
-   trim : true,
+    type: String,
+    required: true,
+    trim : true,
   },
   availability: {
-   type: String,
+    type: String,
   },
-  user: {type: Schema.Types.ObjectId, ref: "user"},
 });
 
-async function findOneOrCreate(profileId){
-  const record = await this.findOne({ profileId });
-  if (record) {
-    return record;
-  } else {
-    return this.create({ profileId });
-  }
-};
-
-ProfileSchema.statics.findOneOrCreate = findOneOrCreate;
-
-module.exports = mongoose.model('Profile',ProfileSchema);
+module.exports = mongoose.model('profile', profileSchema);
