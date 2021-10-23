957a4a14346fbd182c665431c4242565c7eba837
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-23 02:33:22 +0100

    Endpoints adjustment and PATCH method used

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -3,7 +3,7 @@ const Profile = require("../models/Profile");
 const asyncHandler = require("express-async-handler");
 
 exports.allProfiles = asyncHandler(async (req, res, next) => {
-  await Profile.find((err, profiles) => {
+  const profiles = await Profile.find((err, profiles) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -13,11 +13,14 @@ exports.allProfiles = asyncHandler(async (req, res, next) => {
 });
 
 exports.getProfileById = asyncHandler(async (req, res, next) => {
-  const profileId = req.params.id 
-  await Profile.findById(profileId, (profile) => {
-    if (!mongoose.Types.ObjectId.isValid(profileId)) {
+  const profileId = req.params.id
+  if (!mongoose.Types.ObjectId.isValid(profileId)) {
     return res.status(404).send("Profile not found");
-    }  else {
+  } 
+  const profile = await Profile.findById(profileId, (err, profile) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
       res.status(200).json(profile);
     }
   });
@@ -26,6 +29,10 @@ exports.getProfileById = asyncHandler(async (req, res, next) => {
 exports.addProfile = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, gender, dateOfBirth, phone, address, 
     description } = req.body
+  if (!firstName || !lastName || !gender || !dateOfBirth || !phone 
+      || !address || !description) {
+    return res.status(400).send("missing required fields");
+  }
   const newProfile = await new Profile({ firstName, lastName, gender, 
     dateOfBirth, phone, address, description });
   newProfile.save((err, profile) => {
@@ -41,17 +48,20 @@ exports.updateProfile = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, gender, dateOfBirth, phone, address, 
     description } = req.body
   const profileId = req.params.id
-  await Profile.findByIdAndUpdate(
+  if (!mongoose.Types.ObjectId.isValid(profileId)) {
+    return res.status(400).send("Bad Request");
+  }
+  const profile = await Profile.findByIdAndUpdate(
     profileId,
     { firstName, lastName, gender, dateOfBirth, phone, address, 
       description },
     {new: true},
-    (profile) => {
-    if (!mongoose.Types.ObjectId.isValid(profileId)) {
-    return res.status(400).send("Bad Request");
-    } else {
+    (err, profile) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
         res.status(200).json(profile);
       }
-    } 
+    }
   );
 });
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -14,6 +14,6 @@ router.route("/:id").get(getProfileById);
 
 router.route("/").post(protect, validateProfile, addProfile);
 
-router.route("/:id").put(protect, updateProfile);
+router.route("/:id").patch(protect, updateProfile);
 
 module.exports = router;
