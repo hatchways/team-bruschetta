09f8cf4625fb6fbfba25d3ef3d09e4db83338411
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-25 01:12:41 +0100

    Codes and Comments Clean up

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -18,7 +18,6 @@ export const AuthContext = createContext<IAuthContext>({
 });
 
 export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
-  // default undefined before loading, once loaded provide user or null if logged out
   const [loggedInUser, setLoggedInUser] = useState<User | null | undefined>();
   const history = useHistory();
 
@@ -31,7 +30,6 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
   );
 
   const logout = useCallback(async () => {
-    // needed to remove token cookie
     await logoutAPI()
       .then(() => {
         history.push('/login');
@@ -40,7 +38,6 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       .catch((error) => console.error(error));
   }, [history]);
 
-  // use our cookies to check if we can login straight away
   useEffect(() => {
     const checkLoginWithCookies = async () => {
       await loginWithCookies().then((data: AuthApiData) => {
@@ -48,7 +45,6 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
           updateLoginContext(data.success);
           history.push('/dashboard');
         } else {
-          // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
           history.push('/login');
         }
