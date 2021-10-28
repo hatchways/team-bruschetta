318f547b3b13e99ad3d7632d6b56537e7ce49ecc
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-29 00:43:10 +0100

    Images imported and details grid completed

--- client/src/pages/Profile/ProfileDetailPage/ProfileDetail.tsx
+++ client/src/pages/Profile/ProfileDetailPage/ProfileDetail.tsx
@@ -1,21 +1,15 @@
 import * as React from 'react';
 import 'date-fns';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import {
-  Button,
-  TextField,
-  TextareaAutosize,
-  Grid,
-  Box,
-  Typography,
-  FormControl,
-  Container,
-  InputLabel,
-} from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import { Button, TextField, Grid, Box, Typography, Container, Avatar, InputLabel } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import avatar from '../../../Images/68f55f7799df6c8078a874cfe0a61a5e6e9e1687.png';
+import dog_1 from '../../../Images/dog_1.jpeg';
+import dog_2 from '../../../Images/dog_2.jpeg';
+
 import useStyles from './useStyles';
 
 const theme = createMuiTheme();
@@ -25,8 +19,37 @@ export default function ProfileDetail(): JSX.Element {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="md">
-        ghh
+      <Container component="main" maxWidth="lg" className={classes.root}>
+        <Grid item xs={12} sm={8} elevation={6} component={Paper} spacing={3} square className={classes.profile}>
+          <Box className={classes.profileBgImage}>
+            <Avatar alt="Remy Sharp" src={avatar} className={classes.avatar} />
+          </Box>
+          <Box className={classes.details}>
+            <Typography className={classes.name}>Andela Morales</Typography>
+            <Typography className={classes.status}>Lovely pet sitter</Typography>
+            <Typography className={classes.location}>
+              <LocationOnIcon className={classes.locationLogo} />
+              Toronto Ontario
+            </Typography>
+          </Box>
+          <Box className={classes.description}>
+            <Typography className={classes.aboutTitle}>About Me</Typography>
+            <Typography className={classes.about}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima esse maxime voluptatibus veniam officiis
+              laboriosam asperiores repellendus quasi. Corporis, molestias dignissimos. Ipsa, aut et. Debitis aut
+              recusandae exercitationem quia voluptas. Provident porro expedita aperiam aspernatur non enim, eius ipsum
+            </Typography>
+          </Box>
+          <Box className={classes.petName}>
+            <img alt="dog" src={dog_1} className={classes.petItems} />
+            <img alt="dog" src={dog_2} className={classes.petItems} />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.request} spacing={3}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, omnis nobis autem, aut dolorem rerum
+          impedit et atque eligendi architecto numquam, doloribus eaque labore veritatis veniam temporibus nihil qui
+          nesciunt.
+        </Grid>
       </Container>
     </ThemeProvider>
   );
--- client/src/pages/Profile/ProfileDetailPage/useStyles.ts
+++ client/src/pages/Profile/ProfileDetailPage/useStyles.ts
@@ -1,10 +1,80 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { Height } from '@material-ui/icons';
+import profileDetailImage from '../../../Images/profile_detail_bg.jpeg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-    paddingLeft: '29%',
+    '& .MuiInput-underline:before': {
+      borderBottom: '1.2px solid rgba(0, 0, 0, 0.2)',
+    },
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '2rem',
+  },
+  profile: {
+    width: '65%',
+    borderRadius: '10px',
+  },
+  profileBgImage: {
+    backgroundImage: `url(${profileDetailImage})`,
+    height: '40vh',
+    backgroundPosition: 'bottom',
+    backgroundRepeat: 'no-repeat',
+    borderRadius: '10px',
+  },
+  avatar: {
+    width: '15%',
+    height: '30vh',
+    position: 'absolute',
+    left: '23rem',
+    top: '13rem',
+    border: '10px solid #fff',
+  },
+  details: {
+    marginTop: '8rem',
+    marginLeft: '20rem',
+  },
+  name: {
+    marginLeft: '1.2rem',
+    fontWeight: 'bolder',
+    fontSize: '2rem',
+  },
+  status: {
+    fontSize: '0.8rem',
+    color: '#aaa',
+    marginLeft: '4rem',
+  },
+  locationLogo: {
+    color: 'Red',
+  },
+  location: {
+    fontSize: '0.8rem',
+    color: '#aaa',
+    marginTop: '0.5rem',
+    marginLeft: '3rem',
+  },
+  description: {
+    marginTop: '4rem',
+  },
+  aboutTitle: {
+    fontWeight: 'bolder',
+    paddingLeft: '2rem',
+  },
+  about: {
+    fontSize: '0.9rem',
+    lineHeight: '2',
+    padding: '0 3rem 2rem 2rem',
+  },
+  petName: {
+    padding: '0 3rem 2rem 2rem',
+  },
+  petItems: {
+    paddingRight: '2rem',
+    width: '20%',
+    Height: '15vh',
+  },
+  request: {
+    width: '20%',
   },
 }));
 
