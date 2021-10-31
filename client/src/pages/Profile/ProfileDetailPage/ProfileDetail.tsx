00ed5abc8d587bba43a3b7b168b8961049be98b8
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-10-31 21:16:09 +0100

    createMuiTheme and ThemeProvider Deleted

--- client/src/pages/Profile/ProfileDetailPage/ProfileDetail.tsx
+++ client/src/pages/Profile/ProfileDetailPage/ProfileDetail.tsx
@@ -5,13 +5,10 @@ import { Button, Grid, Box, Typography, Container, Avatar, InputLabel } from '@m
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import avatar from '../../../Images/68f55f7799df6c8078a874cfe0a61a5e6e9e1687.png';
 
 import useStyles from './useStyles';
-
-const theme = createMuiTheme();
 
 const initialValues = {
   firstName: 'Andela',
@@ -44,62 +41,60 @@ export default function ProfileDetail(): JSX.Element {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="lg" className={classes.root}>
-        <Grid item xs={12} sm={8} elevation={6} component={Paper} spacing={3} square className={classes.profile}>
-          <Box className={classes.profileBgImage}>
-            <Avatar alt="Remy Sharp" src={avatar} className={classes.avatar} />
-          </Box>
-          <Box className={classes.details}>
-            <Typography className={classes.name}>
-              {initialValues.firstName} {initialValues.lastname}
-            </Typography>
-            <Typography className={classes.status}>{initialValues.status}</Typography>
-            <Typography className={classes.location}>
-              <LocationOnIcon className={classes.locationLogo} />
-              {initialValues.location}
-            </Typography>
-          </Box>
-          <Box className={classes.description}>
-            <Typography className={classes.aboutTitle}>About Me</Typography>
-            <Typography className={classes.about}>{initialValues.description}</Typography>
-          </Box>
-          <Box className={classes.petName}>
-            {initialValues.dogs.map((item) => (
-              <img key={item.img} alt={item.title} src={item.img} className={classes.petItems} />
-            ))}
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={3} elevation={6} component={Paper} spacing={3} className={classes.request}>
-          <Typography className={classes.price}>${initialValues.price}/hr</Typography>
-          <Box component="fieldset" mb={1} className={classes.rating}>
-            <Rating name="read-only" value={value} readOnly />
-          </Box>
-          <InputLabel className={classes.label}>Drop In</InputLabel>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <DateTimePicker
-              name="availability"
-              inputVariant="outlined"
-              value={dropIn}
-              onChange={handleDropIn}
-              className={classes.datePicker}
-            />
-          </MuiPickersUtilsProvider>
-          <InputLabel className={classes.label}>Drop Off</InputLabel>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <DateTimePicker
-              inputVariant="outlined"
-              name="availability"
-              value={dropOff}
-              onChange={handleDropOff}
-              className={classes.datePicker}
-            />
-          </MuiPickersUtilsProvider>
-          <Button type="submit" size="large" variant="contained" color="secondary" className={classes.button}>
-            Send Request
-          </Button>
-        </Grid>
-      </Container>
-    </ThemeProvider>
+    <Container component="main" maxWidth="lg" className={classes.root}>
+      <Grid item xs={12} sm={8} elevation={6} component={Paper} spacing={3} square className={classes.profile}>
+        <Box className={classes.profileBgImage}>
+          <Avatar alt="Remy Sharp" src={avatar} className={classes.avatar} />
+        </Box>
+        <Box className={classes.details}>
+          <Typography className={classes.name}>
+            {initialValues.firstName} {initialValues.lastname}
+          </Typography>
+          <Typography className={classes.status}>{initialValues.status}</Typography>
+          <Typography className={classes.location}>
+            <LocationOnIcon className={classes.locationLogo} />
+            {initialValues.location}
+          </Typography>
+        </Box>
+        <Box className={classes.description}>
+          <Typography className={classes.aboutTitle}>About Me</Typography>
+          <Typography className={classes.about}>{initialValues.description}</Typography>
+        </Box>
+        <Box className={classes.petName}>
+          {initialValues.dogs.map((item) => (
+            <img key={item.img} alt={item.title} src={item.img} className={classes.petItems} />
+          ))}
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={3} elevation={6} component={Paper} spacing={3} className={classes.request}>
+        <Typography className={classes.price}>${initialValues.price}/hr</Typography>
+        <Box component="fieldset" mb={1} className={classes.rating}>
+          <Rating name="read-only" value={value} readOnly />
+        </Box>
+        <InputLabel className={classes.label}>Drop In</InputLabel>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DateTimePicker
+            name="availability"
+            inputVariant="outlined"
+            value={dropIn}
+            onChange={handleDropIn}
+            className={classes.datePicker}
+          />
+        </MuiPickersUtilsProvider>
+        <InputLabel className={classes.label}>Drop Off</InputLabel>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <DateTimePicker
+            inputVariant="outlined"
+            name="availability"
+            value={dropOff}
+            onChange={handleDropOff}
+            className={classes.datePicker}
+          />
+        </MuiPickersUtilsProvider>
+        <Button type="submit" size="large" variant="contained" color="secondary" className={classes.button}>
+          Send Request
+        </Button>
+      </Grid>
+    </Container>
   );
 }
