1e79ef87d50a8c7311d66d0673975131d2e2876a
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-11-03 16:09:41 +0100

    await redundancy removed from controllers

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -3,7 +3,7 @@ const Requests = require('../models/requests');
 const asyncHandler = require("express-async-handler");
 
 exports.allRequests = asyncHandler(async (req, res, next) => {
-  const requests = await Request.find((err, requests) => {
+  const requests = await Request.find((err) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -17,7 +17,7 @@ exports.getRequestById = asyncHandler(async (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(userId)) {
     return res.status(400).send("Bad Request");
   } 
-  const request = await Requests.findById(userId, (err, request) => {
+  const request = await Requests.findById(userId, (err) => {
     if (err) {
       res.status(404).send(err);
     } else {
@@ -32,7 +32,7 @@ exports.addRequest = asyncHandler(async (req, res, next) => {
     return res.status(400).send("missing required fields");
   }
   const newRequest = new Requests({ startDate, endDate, rating});
-    await newRequest.save((err, request) => {
+   const request = await newRequest.save((err) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -54,7 +54,7 @@ exports.updateRequest = asyncHandler(async (req, res, next) => {
     sitterId,
     { accepted, declined },
     {new: true},
-    (err, request) => {
+    (err) => {
       if (err) {
         res.status(500).send(err);
       } else {
