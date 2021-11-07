79aa7ab3393956a788d57dd202811d773d057758
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-11-07 09:16:28 +0100

    Sidebar Componet Created for Dashboard

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
-import { Grid, Box, Typography } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
-import { NavLink, Switch, Route, useHistory } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
@@ -11,6 +11,7 @@ import ProfileSettings from '../ProfileSettings/ProfileSettings';
 import Bookings from '../Bookings/Bookings';
 import SearchProfile from '../Profile/ProfileList/SearchProfile';
 import ProfileDetail from '../Profile/ProfileDetailPage/ProfileDetail';
+import Sidebar from './Sidebar';
 
 import useStyles from './useStyles';
 
@@ -40,35 +41,7 @@ export default function Dashboard({ handleSubmit }: Props): JSX.Element {
     <Box>
       <DashHeader loggedInUser={loggedInUser} />
       <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
-        <Grid xs={12} sm={3} item className={classes.sidebar}>
-          <NavLink to="/dashboard/edit-profile" className={classes.navlink}>
-            <Typography className={classes.links}>Edit Profile</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/settings" className={classes.navlink}>
-            <Typography className={classes.links}>Profile Photo</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/#" className={classes.navlink}>
-            <Typography className={classes.links}>Availability</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/#" className={classes.navlink}>
-            <Typography className={classes.links}>Payment</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/#" className={classes.navlink}>
-            <Typography className={classes.links}>Security</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/#" className={classes.navlink}>
-            <Typography className={classes.links}>Settings</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/manage-bookings" className={classes.navlink}>
-            <Typography className={classes.links2}>Bookings</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/profile-list" className={classes.navlink}>
-            <Typography className={classes.links2}>Profile Listings</Typography>
-          </NavLink>
-          <NavLink to="/dashboard/profile-detail" className={classes.navlink}>
-            <Typography className={classes.links2}>Profile Details</Typography>
-          </NavLink>
-        </Grid>
+        <Sidebar />
         <Grid xs={12} sm={9} component={Paper} spacing={3} square item className={classes.drawerWrapper}>
           <Switch>
             <Route path="/dashboard/edit-profile">
